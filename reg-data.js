023a3ecae7484454fd81e2763d3d72da0b72b76b
kishan yadav <kishany980>
2020-08-11 17:26:01 +0530

    commint for confing

--- reg-data.js
+++ reg-data.js
@@ -1,2 +1,9 @@
 const login ="hello this a is register page";
+const login ="hello this a is header page";const login ="hello this a is register page";
+const login ="hello this a is header page";
+const login ="hello this a is register page";
+const login ="hello this a is header page";
+const login ="hello this a is register page";
+const login ="hello this a is header page";
+const login ="hello this a is register page";
 const login ="hello this a is header page";
